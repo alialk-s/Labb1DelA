07170fa2777d632d432a7a37bec25afc9168c87e
alialk-s <78077817+alialk-s@users.noreply.github.com>
2021-02-04 17:44:38 +0100

    Add files via upload

--- Truck.java
+++ Truck.java
@@ -2,6 +2,13 @@ package bobo;
 
 import java.security.InvalidParameterException;
 
+/**
+ * A class, subclass of Car, represents a truck and is inherited by different types of truck
+ * <p></p>
+ * @author Ali Alkhaled
+ * @since 2021-02-01
+ * @version 1.0
+ */
 public abstract class Truck extends Car {
 
     private double platformAngle;
@@ -10,24 +17,35 @@ public abstract class Truck extends Car {
         super(enginePower, nrDoors, modelName);
     }
 
+    /**
+     * A method to get the value of platform's angle
+     * @return platformAngle
+     */
     public double getPlatformAngle() {
         return platformAngle;
     }
 
-    public void setPlatformAngle(double platformAngle) {
-        this.platformAngle = platformAngle;
-    }
 
+    /**
+     * A method to get the speed factor of a truck
+     * @return
+     */
     @Override
     protected double speedFactor() {
         return 0;
     }
 
 
+    /**
+     * A method to (re)set the value of platform's angle where the value must be with in the interval 0 - 70 degree
+     * <p>
+     * Also, the truck must be in sleep mode, i.e. its speed must be zero, to be able to change platform's position
+     * @param angle double
+     */
     public void changePlatformsPosition(double angle) {
 
         if (getCurrentSpeed() == 0) {
-            if (angle > 0 && angle <= 70) {
+            if (angle >= 0 && angle <= 70) {
                 platformAngle = angle;
             } else {
                 throw new InvalidParameterException("Invalid Angle");
